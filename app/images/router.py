4ecaaa35ef3efe62a99d556807c0a25e73f3f190
alonskyi <amatorspb@gmail.com>
2026-01-14 12:56:35 +0500

    Добавил Воркера для обработок Фоток[celery]

--- app/images/router.py
+++ app/images/router.py
@@ -1,7 +1,7 @@
 from fastapi import UploadFile, APIRouter
 import  shutil
 
-
+from app.tasks.tasks import process_pic
 
 router = APIRouter(
     prefix="/images",
@@ -11,5 +11,7 @@ router = APIRouter(
 
 @router.post("/skins")
 async def  add_skins_image(name:int ,file: UploadFile ):
-    with open(f"app/static/images/{name}.webp", "wb+") as file_object:
+    im_path = f"app/static/images/{name}.webp"
+    with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(im_path)
